c51fad84e7a946d6bec06041fd9095f5b10548f4
fatih <fthhbsr@gmail.com>
2022-06-05 15:37:04 +0300

    project started

---
+++ index.js
@@ -0,0 +1,9 @@
+const min = 0;
+const max = 100;
+let guess, newmin, newmax;
+randomNum = Math.floor(Math.random() * 100);
+console.log(randomNum);
+
+const guessGame = function(){
+    guess = +
+}
